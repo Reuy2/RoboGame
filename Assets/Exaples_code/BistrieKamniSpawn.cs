b716ac85488795af4c8cda5357acaa2fcc17a68b
Farkval <aminev.farki3@gmail.com>
2022-11-02 09:44:33 +0300

    Анимация разбиение астероидов + двигателя
    изменен полигонколайдер на быстром камне, анимация горения двигателя (не полностью настроена), анимация разбиения астероидов

---
+++ Assets/Exaples_code/BistrieKamniSpawn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BistrieKamniSpawn : MonoBehaviour
+{
+    //[SerializeField] public Camera CurrentCamera = Camera.main;
+    [SerializeField] private GameObject _asteroid;
+    [SerializeField] private float _respawnTime = 0.7f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(asteroidWave());
+    }
+
+    void Update()
+    {
+    }
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(_asteroid);
+        a.transform.position = new Vector2(75, Random.Range(-40, +40));
+    }
+
+    IEnumerator asteroidWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_respawnTime);
+            spawnEnemy();
+        }
+    }
+}
